5cfea8e2ce7816d73d5be667272d83df66e8e839
austinjp <austin.plunkett@gmail.com>
2022-02-07 09:18:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 
 setup(
-    name='text_summarizer',
-    version='0.1',
-    url='https://github.com/lambdaofgod/text-summarizer',
-    author='Jakub Bartczuk',
+    name="centroid_summarizer",
+    version="0.1",
+    url="https://github.com/cordelia-io/centroid-summarizer",
+    author="Jakub Bartczuk, austinjp",
+    maintainer="austinjp",
     packages=find_packages(),
     install_requires=requirements
 )
