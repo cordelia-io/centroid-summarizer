a059cf17933108f6fc10188ef2e6b11f2c88e8fb
austinjp <austin.plunkett@gmail.com>
2022-02-08 12:55:46 +0000

    Add simple_clean utility func.
    Also removed old commented-out class

--- centroid_summarizer/base.py
+++ centroid_summarizer/base.py
@@ -1,7 +1,10 @@
 import logging
 
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords as nltk_stopwords
 from numpy import count_nonzero, dot
 from numpy.linalg import norm
+from unidecode import unidecode
 
 logger = logging.getLogger("centroid_summarizer")
 
@@ -15,6 +18,29 @@ def similarity(v1, v2):
         score = ((1 - cosine(v1, v2)) + 1) / 2
     return score
 
+def simple_clean(
+        text,
+        stopwords=nltk_stopwords.words("english")
+):
+    if type(text) == str:
+        arr = sent_tokenize(text)
+    if not hasattr(text,"__iter__"):
+        raise Exception("Expects a string or an array of strings.")
+    for sentence in arr:
+        if type(sentence) != str:
+            sentence = " ".join(sentence)
+        clean_sentence = []
+        for word in word_tokenize(sentence):
+            clean_word = "".join(
+                letter for letter in unidecode(
+                    str(word).lower()
+                )
+                if letter.isalpha()
+            )
+            if clean_word and clean_word not in stopwords:
+                clean_sentence.append(clean_word)
+        yield " ".join(clean_sentence)
+
 
 default_language = "english"
 default_length_limit = 100
@@ -23,28 +49,3 @@ default_placeholder = "\0" # "###nul###"
 default_remove_stopwords = True
 default_similarity_threshold = 0.95
 default_topic_threshold = 0.3
-
-
-# class BaseSummarizer:
-#     def __init__(
-#             self,
-#             language=default_language,
-#             remove_stopwords=default_remove_stopwords,
-#             stopwords=default_stopwords,
-#             length_limit=default_length_limit,
-#             placeholder=default_placeholder
-#     ):
-#         self.language = language
-#         self.remove_stopwords = remove_stopwords
-#         self.stopwords = stopwords
-#         self.length_limit = length_limit
-#         self.placeholder = placeholder
-#         if remove_stopwords:
-#             stopword_remover = KeywordProcessor()
-#             for stopword in stopwords:
-#                 stopword_remover.add_keyword(stopword, self.placeholder)
-#             self.stopword_remover = stopword_remover
-#         return
-# 
-#     def summarize(self, text, limit=default_length_limit):
-#         raise NotImplementedError()
